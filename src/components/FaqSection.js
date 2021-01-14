38f58c420e63e32a5a188b70b85d6be9dd7a37f8
Arthur Dias <dev.arthur99@gmail.com>
2021-01-14 11:27:19 -0300

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque vero
+            amet distinctio non atque asperiores porro voluptatum reiciendis
+            dolorum, voluptate, error iste sed ex consequatur impedit
+            perferendis dolorem quasi officiis.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque vero
+            amet distinctio non atque asperiores porro voluptatum reiciendis
+            dolorum, voluptate, error iste sed ex consequatur impedit
+            perferendis dolorem quasi officiis.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque vero
+            amet distinctio non atque asperiores porro voluptatum reiciendis
+            dolorum, voluptate, error iste sed ex consequatur impedit
+            perferendis dolorem quasi officiis.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Do You Offer</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque vero
+            amet distinctio non atque asperiores porro voluptatum reiciendis
+            dolorum, voluptate, error iste sed ex consequatur impedit
+            perferendis dolorem quasi officiis.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
